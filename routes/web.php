8e0cfe60ac4cbe4c5c69a8679e5efd22506ba55a
Andi Fajar Nugraha <andifajarlah@gmail.com>
2025-04-20 17:21:22 +0800

    privacypolicy/catat

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class);
 Route::get('/kontak', Kontak::class);
+Route::get('/privacypolicy/catat', function () {
+    return view('privacypolicy.catat');
+});
