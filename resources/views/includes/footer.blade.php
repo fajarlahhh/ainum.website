0b44a02cab285be92a40884bba580dd22908e7d5
Andi Fajar Nugraha <andifajarlah@gmail.com>
2025-06-12 15:41:09 +0800

    fa

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,99 +1,89 @@
-
-    <!-- Footer Section Start -->
-    <div class="footer">
-        <div class="container">
-            <div class="row g-0">
-                <div class="col-lg-4 col-md-5">
-                    <div class="footer-left">
-                        <div class="footer-left-inner">
-                            <div class="footer-left-logo">
-                                <a href="index.html">
-                                    <img src="assets/images/logo.png" alt="LogoImage">
-                                </a>
-                            </div>
-                            <p class="footer-left-text">Fectetur adipisicing elit, sed do eius mod tempor incididu onsectetur adipisicing eli mod tempor</p>
-                            <ul class="footer-left-list-icon">
-                                <li><i class="fas fa-map-marker-alt"></i> <span> 20 Bordeshi, Amin Bazar <br> Savar, Dhaka 1348</span></li>
-                                <li><i class="fas fa-envelope"></i> <span>hello@egens.com</span> </li>
-                                <li><i class="fas fa-phone"></i><span>+123 456 7890</span> </li>
-                            </ul>
-                            <ul class="social-icon d-flex flex-start">
-                                <li>
-                                    <a href="https://www.facebook.com/" class="facebook"><i class="fab fa-facebook-f"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://twitter.com/" class="twitter"><i class="fab fa-twitter"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://www.linkedin.com/" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
-                                </li>
-                                <li>
-                                    <a href="https://www.instagram.com/" class="instagram"><i class="fab fa-instagram"></i></a>
-                                </li>
-                            </ul>
+<!-- Footer Section Start -->
+<div class="footer">
+    <div class="container">
+        <div class="row g-0">
+            <div class="col-lg-4 col-md-5">
+                <div class="footer-left">
+                    <div class="footer-left-inner" style="margin-bottom: 85px">
+                        <div class="footer-left-logo">
+                            <a href="/">
+                                <img src="assets/images/logo.png" alt="LogoImage">
+                            </a>
                         </div>
-                    </div>
-                </div>
-                <div class="col-lg-8 col-md-7">
-                    <div class="footer-right">
-                        <div class="footer-top">
-                            <!-- Heading Start -->
-                            <div class="heading-one">
-                                <span class="heading-one-subtitle">Design matters</span>
-                                <h2 class="heading-one-title">Lets Start A Project</h2>
-                                <p>Kimod tempoer incididunt onomes</p>
-                            </div>
-                            <!-- Heading End -->
-                            <a href="contact.html" class="btn-style-three"><span>Contact Us</span></a>
-                        </div>
-                        <div class="footer-bottom">
-                            <div class="footer-widget">
-                                <h5 class="footer-widget-title">Links</h5>
-                                <ul class="footer-widget-list">
-                                    <li><a href="contact.html">Customer</a></li>
-                                    <li><a href="contact.html">Visitor</a></li>
-                                    <li><a href="contact.html">Webmaster</a></li>
-                                    <li><a href="contact.html">Service</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer-widget">
-                                <h5 class="footer-widget-title">Help</h5>
-                                <ul class="footer-widget-list">
-                                    <li><a href="contact.html">Support</a></li>
-                                    <li><a href="contact.html">Doc File</a></li>
-                                    <li><a href="contact.html">Forum</a></li>
-                                    <li><a href="contact.html">FAQ</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer-widget-subscription">
-                                <h5 class="footer-widget-title">Subscribe to our newsletter</h5>
-                                <p class="footer-widget-text">Fectetur adipisicing elit, sed do eius mod tempor incididunt </p>
-                                <form action="#" class="footer-widget-form">
-                                    <input type="text" placeholder="Your email here">
-                                    <button><img src="assets/images/icon/icon-5.png" alt="IconImage"></button>
-                                </form>
-                            </div>
-                        </div>
+                        <br>
+                        <br>
+                        <ul class="footer-left-list-icon">
+                            <li><i class="fas fa-map-marker-alt"></i> <span> l. Lingkar Selatan Km. 4 <br> Unter
+                                    Iwis</span></li>
+                            <li><i class="fas fa-envelope"></i> <span>pdam.sumbawa@yahoo.com</span> </li>
+                            <li><i class="fas fa-phone"></i><span>+6285191442092</span> </li>
+                        </ul>
+                        <ul class="social-icon d-flex flex-start">
+                            <li><a href="https://www.facebook.com/rhaden.brighita" class="facebook"><i
+                                        class="fab fa-facebook-f"></i></a></a></li>
+                            <li><a href="https://www.instagram.com/perumdam_batulanteh/" class="instagram"><i
+                                        class="fab fa-instagram"></i></a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="footer-copyright">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 position-relative">
-                        <div class="footer-copyright-inner">
-                            <p class="order-md-1 order-2">Designed with love by <span>© Jewel Khan</span></p>
-
-                            <ul class="footer-bootm-list order-md-2 order-1">
-                                <li><a href="contact.html">Terms &amp; Condition</a></li>
-                                <li><a href="contact.html">Privecy Policy</a></li>
-                                <li><a href="contact.html">Legal</a></li>
+            <div class="col-lg-8 col-md-7">
+                <div class="footer-right">
+                    <div class="footer-top">
+                        <!-- Heading Start -->
+                        <div class="heading-one">
+                            <span class="heading-one-subtitle">Ada pertanyaan?</span>
+                            <h2 class="heading-one-title">Kami siap menjawab</h2>
+                            <p>&nbsp;</p>
+                        </div>
+                        <!-- Heading End -->
+                        <a href="/kontak" class="btn-style-three"><span>Klik di sini</span></a>
+                    </div>
+                    <div class="footer-bottom">
+                        <div class="footer-widget">
+                            <h5 class="footer-widget-title">Pelayanan</h5>
+                            <ul class="footer-widget-list">
+                                <li><a href="/pengaduan">Pengaduan</a></li>
+                                <li><a href="/infotagihan">Info Tagihan</a></li>
+                                <li><a href="/pembangunan">Pasang Baru</a></li>
+                                <li><a href="/laporangka">Laporan Angka</a></li>
                             </ul>
+                        </div>
+                        <div class="footer-widget">
+                           &nbsp;
+                        </div>
+                        <div class="footer-widget-two">
+                            <div class="row">
+                                @foreach (\App\Models\Mitra::all() as $mitra)
+                                    <div class="col-lg-4">
+                                        <a href="#">
+                                            <img src="{{ Storage::url($mitra->foto1) }}">
+                                        </a>
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Footer Section End -->
+    <div class="footer-copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 position-relative">
+                    <div class="footer-copyright-inner">
+                        <p class="order-md-1 order-2">Designed by <span>© PBL 2025</span></p>
+
+                        <ul class="footer-bootm-list order-md-2 order-1">
+                            <li><a href="javascript:void(0)">Terms &amp; Condition</a></li>
+                            <li><a href="javascript:void(0)">Privecy Policy</a></li>
+                            <li><a href="javascript:void(0)">Legal</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer Section End -->
